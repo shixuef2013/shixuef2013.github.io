8a1e13bf5e6ddf2c529424e684592d33386f1fd3
shixuefeng <shixuef2013@163.com>
2018-08-26 19:17:31 +0800

    add login and register page

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.gif') {
-      myImage.setAttribute('src', 'images/firefox-icon.jpg');
+    if (mySrc === 'images/firefox-icon.gif') {
+        myImage.setAttribute('src', 'images/firefox-icon.jpg');
     } else {
-      myImage.setAttribute('src', 'images/firefox-icon.gif');
+        myImage.setAttribute('src', 'images/firefox-icon.gif');
     }
 }
 
@@ -13,18 +13,18 @@ var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
 
 function setUserName() {
-  var myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
 }
 
-if(!localStorage.getItem('name')) {
-  setUserName();
+if (!localStorage.getItem('name')) {
+    setUserName();
 } else {
-  var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
 }
 
 myButton.onclick = function() {
-  setUserName();
+    setUserName();
 }
